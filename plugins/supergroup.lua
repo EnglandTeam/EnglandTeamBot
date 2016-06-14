f17140594c8e01cd44b86191b96f494fb9f5ba1a
mrmhxx82 <learingcreateantispambot@gmail.com>
2016-06-15 02:11:07 +0430

    Update supergroup.lua

--- plugins/supergroup.lua
+++ plugins/supergroup.lua
@@ -568,7 +568,7 @@ end
  local url , res = http.request('http://api.gpmod.ir/time/')
   if res ~= 200 then return "No connection" end
   local jdat = json:decode(url)
-  local text = "🇬🇧 Group Name:"..msg.to.print_name.."\n🇬🇧 Group Settings: \n🔹🔸 Lock links : "..settings.lock_link.."\n🔹🔸 Lock flood: "..settings.flood.."\n🔹🔸 Flood sensitivity : "..NUM_MSG_MAX.."\n🔹🔸 Lock spam: "..settings.lock_spam.."\n🔹🔸 Lock Arabic: "..settings.lock_arabic.."\n🔹🔸 Lock Member: "..settings.lock_member.."\n🔹🔸 Lock RTL: "..settings.lock_rtl.."\n🔹🔸 Lock Tgservice : "..settings.lock_tgservice.."\n🔹🔸 Lock sticker: "..settings.lock_sticker.."\n🔹🔸 Public: "..settings.public.."\n🔹🔸 Strict settings: "..settings.strict.."\n-----------------------------\n⭕️ Group Id: "..msg.to.id.."\n⭕️ Your Id: "..msg.from.id.."\n-----------------------------\n⏱ Time For Request: "..jdat.ENdate
+local text = "🇬🇧 Group Name:"..msg.to.print_name.."\n\n🇬🇧 Group Settings: \n\n🔹🔸 Lock links : "..settings.lock_link.."\n🔹🔸 Lock flood: "..settings.flood.."\n🔹🔸 Flood sensitivity : "..NUM_MSG_MAX.."\n🔹🔸 Lock spam: "..settings.lock_spam.."\n🔹🔸 Lock Arabic: "..settings.lock_arabic.."\n🔹🔸 Lock Member: "..settings.lock_member.."\n🔹🔸 Lock RTL: "..settings.lock_rtl.."\n🔹🔸 Lock Tgservice : "..settings.lock_tgservice.."\n🔹🔸 Lock sticker: "..settings.lock_sticker.."\n🔹🔸 Public: "..settings.public.."\n🔹🔸 Strict settings: "..settings.strict.."\n-----------------------------\n⭕️ Group Id: "..msg.to.id.."\n⭕️ Your Id: "..msg.from.id.."\n-----------------------------\n<code>⏱ Time For Request: "..jdat.ENdate.."</code>"
   return text
 end
 
@@ -1967,7 +1967,7 @@ local function run(msg, matches)
 		end
 
 		if matches[1] == 'help' and not is_owner(msg) then
-			text = "Message /superhelp to @Teleseed in private for SuperGroup help"
+			text = ""
 			reply_msg(msg.id, text, ok_cb, false)
 		elseif matches[1] == 'help' and is_owner(msg) then
 			local name_log = user_print_name(msg.from)
