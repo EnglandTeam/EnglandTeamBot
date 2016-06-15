348185e1f6a35c3f34ccbad0834a362ffab7074e
mrmhxx82 <learingcreateantispambot@gmail.com>
2016-06-15 05:17:21 +0430

    Update id.lua

--- plugins/id.lua
+++ plugins/id.lua
@@ -1,7 +1,7 @@
 local function run(msg, matches)
 
 if matches[1]:lower() == 'id' then
-return '🇬🇧 Id For You And Group \n <code>🔻 Group Id: '..msg.to.id..'\n🔻 Group Name: '..msg.to.print_name..'\n\n🔻 Your Id: '..msg.from.id..'\n🔻 Your Username: @'..msg.from.username..'</code>'
+return '🇬🇧 Id For You And Group \n🔻 Group Id: '..msg.to.id..'\n🔻 Group Name: '..msg.to.print_name..'\n\n🔻 Your Id: '..msg.from.id..'\n🔻 Your Username: @'..msg.from.username
 end
 end
 
