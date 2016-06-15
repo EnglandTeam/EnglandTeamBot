0f986107d942b4d541144a3e99b053902a97b115
mrmhxx82 <learingcreateantispambot@gmail.com>
2016-06-16 02:43:33 +0430

    Update plugins.lua

--- plugins/plugins.lua
+++ plugins/plugins.lua
@@ -1,5 +1,5 @@
 do
-
+--create by RoyalTeam ID CHANNEL : @RoyalTeamCh
 -- Returns the key (index) in the config.enabled_plugins table
 local function plugin_enabled( name )
   for k,v in pairs(_config.enabled_plugins) do
@@ -24,15 +24,15 @@ end
 local function list_plugins(only_enabled)
   local text = ''
   for k, v in pairs( plugins_names( )) do
-    --  ✔ enabled, ❌ disabled
-    local status = '❌'
+    --  ➗ enabled, ➖ disabled
+    local status = '➖'
     -- Check if is enabled
     for k2, v2 in pairs(_config.enabled_plugins) do
       if v == v2..'.lua' then 
-        status = '✔' 
+        status = '➗' 
       end
     end
-    if not only_enabled or status == '✔' then
+    if not only_enabled or status == '➗' then
       -- get the name
       v = string.match (v, "(.*)%.lua")
       text = text..v..'  '..status..'\n'
@@ -99,7 +99,7 @@ local function disable_plugin_on_chat(receiver, plugin)
   _config.disabled_plugin_on_chat[receiver][plugin] = true
 
   save_config()
-  return 'Plugin '..plugin..' disabled on this chat'
+  return 'Plugin '..plugin..' disabled on this supergroup'
 end
 
 local function reenable_plugin_on_chat(receiver, plugin)
@@ -108,7 +108,7 @@ local function reenable_plugin_on_chat(receiver, plugin)
   end
 
   if not _config.disabled_plugin_on_chat[receiver] then
-    return 'There aren\'t any disabled plugins for this chat'
+    return 'There aren\'t any disabled plugins for this supergroup'
   end
 
   if not _config.disabled_plugin_on_chat[receiver][plugin] then
@@ -122,27 +122,27 @@ end
 
 local function run(msg, matches)
   -- Show the available plugins 
-  if matches[1] == '!plugins' then
+  if matches[1] == '/plist' then
     return list_plugins()
   end
 
   -- Re-enable a plugin for this chat
-  if matches[1] == 'enable' and matches[3] == 'chat' then
+  if matches[1] == '+' and matches[3] == 'supergroup' then
     local receiver = get_receiver(msg)
     local plugin = matches[2]
-    print("enable "..plugin..' on this chat')
+    print("enable "..plugin..' on this supergroup')
     return reenable_plugin_on_chat(receiver, plugin)
   end
 
   -- Enable a plugin
-  if matches[1] == 'enable' then
+  if matches[1] == '+' then
     local plugin_name = matches[2]
     print("enable: "..matches[2])
     return enable_plugin(plugin_name)
   end
 
   -- Disable a plugin on a chat
-  if matches[1] == 'disable' and matches[3] == 'chat' then
+  if matches[1] == '-' and matches[3] == 'supergroup' then
     local plugin = matches[2]
     local receiver = get_receiver(msg)
     print("disable "..plugin..' on this chat')
@@ -150,17 +150,17 @@ local function run(msg, matches)
   end
 
   -- Disable a plugin
-  if matches[1] == 'disable' then
+  if matches[1] == '-' then
     print("disable: "..matches[2])
     return disable_plugin(matches[2])
   end
 
   -- Reload all the plugins!
-  if matches[1] == 'reload' then
+  if matches[1] == '*' then
     return reload_plugins(true)
   end
 end
-
+--create by RoyalTeam ID CHANNEL : @RoyalTeamCh
 return {
   description = "Plugin to manage other plugins. Enable, disable or reload.", 
   usage = {
@@ -170,12 +170,12 @@ return {
     "!plugins disable [plugin] chat: disable plugin only this chat.",
     "!plugins reload: reloads all plugins." },
   patterns = {
-    "^!plugins$",
-    "^!plugins? (enable) ([%w_%.%-]+)$",
-    "^!plugins? (disable) ([%w_%.%-]+)$",
-    "^!plugins? (enable) ([%w_%.%-]+) (chat)",
-    "^!plugins? (disable) ([%w_%.%-]+) (chat)",
-    "^!plugins? (reload)$" },
+    "^[/!#]plist$",
+    "^[/#!]pl? (+) ([%w_%.%-]+)$",
+    "^[/#!]pl? (-) ([%w_%.%-]+)$",
+    "^[/#!]pl? (+) ([%w_%.%-]+) (supergroup)",
+    "^[/#!]pl? (-) ([%w_%.%-]+) (supergroup)",
+    "^[/#!]pl? (*)$" },
   run = run,
   privileged = true
 }
