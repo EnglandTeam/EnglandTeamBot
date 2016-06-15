b470564cfc81c220fc1af3059c046978063bcd2f
mrmhxx82 <learingcreateantispambot@gmail.com>
2016-06-15 20:01:58 +0430

    Create help.lua

---
+++ plugins/help.lua
@@ -0,0 +1,21 @@
+local function run9msg, matches)
+
+if is_momod(msg)
+if matches[1]:lower() == 'help' then
+return '🇬🇧 Help For Owner \n #help_fun \n <code> 🇬🇧 Get Help For Tools \n\n #help_manager \n <code> 🇬🇧 Get Help For Group Manager </code>'
+end
+end
+end
+
+return {
+
+patterns = {
+
+"[!#+/$*]([Hh][Ee][Ll][Pp]",
+
+
+},
+
+run = run
+
+}
